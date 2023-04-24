727388fe1658c929432f1a570eea968e089eff1a
Harsh Bansal <harshbansal1717@gmail.com>
2023-04-24 23:04:31 +0530

    updating_Databases

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://0.0.0.0:27017/blogDB");
+
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
 
 var _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "This is Daly Journal webpage. Here You can write anything or share latest news about various events Happening in the country. It's Like a Notice Board where you can post anything";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const arr = [];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,44 +27,49 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const Post = mongoose.model('Post', postSchema);
+
+
 app.get("/", function (req, res) {
-  res.render('home', { homecontent: homeStartingContent, arr: arr });
+  Post.find({})
+    .then(data => {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: data
+      });
+    });
 })
 app.get("/about", function (req, res) {
-  res.render('about', { aboutcontent: aboutContent });
+  res.render('about', { aboutContent: aboutContent });
 })
 app.get("/contact", function (req, res) {
-  res.render('contact', { contactcontent: contactContent });
+  res.render('contact', { contactContent: contactContent });
 })
 app.get("/compose", function (req, res) {
   res.render('compose');
 })
 
-app.get("/post/:topic", function (req, res) {
-  const requestTitle = _.lowerCase(req.params.topic);
-  var count = -1;
-  // for (let i = 0; i < arr.length; i++) {
-  //   const element = arr[i];
-  //   if (element.title === requestTitle) {
-  //     count = 0;
+app.get("/posts/:postID", function (req, res) {
+  const requestedPostId = req.params.postID;
 
-  //   }
 
-  // }
-  arr.forEach(function (post) {
-    const element = _.lowerCase(post.title);
-    if (element === requestTitle) {
-      res.render("post", { title: post.title, content: post.content });
-    }
-  })
+  Post.findOne({ _id: requestedPostId })
+    .then(data => {
+      res.render("post", {
+        title: data.title,
+        content: data.content
+      });
+    });
 
-})
+});
 app.post("/compose", function (req, res) {
-  const val = {
-    title: req.body.titleContent,
-    content: req.body.postContent
-  };
-  arr.push(val);
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save();
+
   res.redirect("/");
 })
 
