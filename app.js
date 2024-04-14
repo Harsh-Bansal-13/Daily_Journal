424bbe6bbcc3b5a285b13fee479c43ea3665ee93
Harsh-Bansal-13 <111892307+Harsh-Bansal-13@users.noreply.github.com>
2024-04-14 14:24:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const postSchema = new mongoose.Schema({
 var _ = require("lodash");
 
 const homeStartingContent =
-  "This is Daly Journal webpage. Here You can write anything or share latest news about various events Happening in the country. It's Like a Notice Board where you can post anything";
+  "This is Daily Journal webpage. Here You can write anything or share latest news about various events Happening in the country. It's Like a Notice Board where you can post anything";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
